0856f1296e6b53b7374d0dc83d2e3eeb884d18e0
mirzanahid <mirzasaikat3@gmail.com>
2022-09-28 21:48:52 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Exercises from './components/Exercises/Exercises';
+
+function App() {
+  return (
+    <div className="App">
+   <Exercises></Exercises>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Exercises/Exercises.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Exercises.css';
+
+const Exercises = () => {
+    return (
+        <div className='exercises'>
+           <a className='logo' href="index.html">FITNESS-GYM</a>
+        </div>
+    );
+};
+
+export default Exercises;
