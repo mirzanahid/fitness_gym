efcd027fda789be27d586db76009dd5a0bddb371
mirzanahid <mirzasaikat3@gmail.com>
2022-09-29 09:50:03 +0600

    gym json data added

--- src/components/Exercise/Exercise.js
+++ src/components/Exercise/Exercise.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import './Exercise.css'
 
-const Exercise = () => {
+const Exercise = ({exercise}) => {
+    console.log(exercise)
+    const {img, title, description, age, duration } = exercise;
     return (
         <div className='exercise'>
-          
-         <div className='exercise-image'>
-         <img src="https://media.istockphoto.com/photos/young-woman-preparing-to-lift-heavy-weights-in-a-gym-picture-id874001794?k=20&m=874001794&s=612x612&w=0&h=WFnx_UkzdnouVcIMDVlgYRh9Qw7gVFl_ZkNDa47ObxI=" alt="" />
-         </div>
-        <div className='exercise-info'>
-        <h2 className='ex-title'>Exercise Title</h2>
-         <p className='exercise-description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, rem culpa repellat suscipit quaerat dignissimos?</p>
-         <div className="required-age">For Age: 20-25</div>
-         <div className="required-time">Time required : <span>30</span>s</div>
-         <button className='add-list-button'>Add to list</button>
-        </div>
+            <div className='exercise-image'>
+                <img src={img} alt="exercise" />
+            </div>
+            <div className='exercise-info'>
+                <h2 className='ex-title'>{title}</h2>
+                <p className='exercise-description'>{description}</p>
+                <div className="required-age">For Age: <span>{age.min}</span> - <span>{age.max}</span> </div>
+                <div className="required-time">Time required : <span>{duration}</span>s</div>
+                <button className='add-list-button'>Add to list</button>
+            </div>
         </div>
     );
 };
--- src/components/Exercises/Exercises.js
+++ src/components/Exercises/Exercises.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Exdetails from '../Exdetails/Exdetails';
 
 import Exercise from '../Exercise/Exercise';
 import './Exercises.css';
 
 const Exercises = () => {
+    const [exercises, setExercises] = useState([]);
+    useEffect(()=>{
+        fetch('gym.json')
+        .then(res => res.json())
+        .then(data => setExercises(data))
+    },[])
     return (
         <div className='exercises'>
            <div className='exercise-main'>
            <div className="exercise-container">
            <a className='logo' href="index.html">FITNESS-GYM</a>
-           <Exercise></Exercise>
+          <div className='all-exercises'>
+          {
+            exercises.map(exercise =><Exercise key={exercise.id} exercise={exercise}></Exercise> )
+           }
+        
+          </div>
             
            </div>
            <div className="exdetalis-container">
